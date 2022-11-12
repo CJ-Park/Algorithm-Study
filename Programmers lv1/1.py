cc7b736c51fed23e9c66c222562a8e721e864472
JOE1534 <king01286@naver.com>
2022-11-12 13:36:34 +0900

    First Commit

---
+++ Programmers lv1/1.py
@@ -0,0 +1,18 @@
+def solution(a, b, n):
+    answer = 0
+    left = 0
+    while(n >= a):
+        if(n % a == 0):
+            answer += (n // a) * b  
+            n = (n // a) * b
+        else:
+            left = n % a
+            answer += (n // a) * b
+            n = (n // a) * b + left
+            left = 0
+            
+    return answer
+
+print(solution(2, 1, 20));
+print(solution(3, 1, 20));
+print(solution(3, 2, 20));
