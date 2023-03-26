22cdb72229bc18e103e1ff1a2d8915800ee9fb85
CJ-Park <king01286@naver.com>
2023-03-26 23:42:38 +0900

    2908 class 1++ 상수

---
+++ BaekJoon/essential/2908.py
@@ -0,0 +1,9 @@
+a,b = map(str, input().split())
+
+for i in range(0,3):
+    if(a[2-i] < b[2-i]):
+        print(b[::-1])
+        break
+    elif(a[2-i] > b[2-i]):
+        print(a[::-1])
+        break
