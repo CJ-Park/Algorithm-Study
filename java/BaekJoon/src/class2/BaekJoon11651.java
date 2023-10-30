c703e8ce6cdff25358bfc3020ccb0941fb96ebb4
CJ-Park <king01286@naver.com>
2023-10-30 09:52:08 +0900

    11651 / class 2 / 좌표 정렬하기 2

---
+++ java/BaekJoon/src/class2/BaekJoon11651.java
@@ -0,0 +1,40 @@
+package class2;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class BaekJoon11651 {
+    public void solution() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+        int N = Integer.parseInt(br.readLine());
+
+        int[][] arr = new int[N][2];
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            arr[i][0] = Integer.parseInt(st.nextToken());
+            arr[i][1] = Integer.parseInt(st.nextToken());
+        }
+
+        // arr[i][1] 기준으로 정렬 후 arr[i][1] 같은 애들은 arr[i][0] 기준으로 마저 정렬
+        // NlogN 시간 복잡도여야 됨 => 머지소트 사용 (Arrays.sort 구현부 확인)
+        Arrays.sort(arr, (a, b) -> {
+            if(a[1] == b[1]) {
+                return a[0] - b[0];
+            } else {
+                return a[1] - b[1];
+            }
+        });
+
+
+        for (int i = 0; i < N; i++) {
+            sb.append(arr[i][0]).append(" ").append(arr[i][1]);
+            System.out.println(sb);
+            sb.setLength(0);
+        }
+    }
+}
