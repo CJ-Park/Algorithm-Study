8d2c3653f107f486629d5033447586282cb991a0
CJ-Park <king01286@naver.com>
2023-11-11 22:43:37 +0900

    집합 / class 3 / 11723

---
+++ java/BaekJoon/src/class2/BaekJoon11723.java
@@ -0,0 +1,59 @@
+package class2;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.StringTokenizer;
+
+// 집합
+public class BaekJoon11723 {
+    int m;
+    Set<Integer> set;
+    BufferedReader br;
+    StringTokenizer st;
+    StringBuilder sb;
+
+    public void solution() throws IOException {
+        br = new BufferedReader(new InputStreamReader(System.in));
+        set = new HashSet<>();
+        m = Integer.parseInt(br.readLine());
+        sb = new StringBuilder();
+
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+            String solve = st.nextToken();
+            getResult(solve);
+        }
+        System.out.print(sb);
+    }
+
+    public void getResult(String s) {
+        switch (s) {
+            case "add" :
+                set.add(Integer.parseInt(st.nextToken()));
+                break;
+            case "remove" :
+                set.remove(Integer.parseInt(st.nextToken()));
+                break;
+            case "check" :
+                if (set.contains(Integer.parseInt(st.nextToken()))) sb.append(1).append("\n");
+                else sb.append(0).append("\n");
+                break;
+            case "toggle" :
+                int value = Integer.parseInt(st.nextToken());
+                if (set.contains(value)) set.remove(value);
+                else set.add(value);
+                break;
+            case "all" :
+                for (int i = 1; i <= 20; i++) {
+                    set.add(i);
+                }
+                break;
+            case "empty" :
+                set.clear();
+                break;
+        }
+    }
+}
