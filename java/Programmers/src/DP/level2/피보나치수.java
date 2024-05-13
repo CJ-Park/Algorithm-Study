8307eea9483879d654cbd6fef4a821075f2015a9
CJ-Park <king01286@naver.com>
2024-05-13 13:36:11 +0900

    [프로그래머스] DP / level2 / 피보나치수

---
+++ java/Programmers/src/DP/level2/피보나치수.java
@@ -0,0 +1,20 @@
+package DP.level2;
+
+// 반복문 사용
+public class 피보나치수 {
+    static int[] fibo = new int[100001];
+    static int NUM = 1234567;
+
+    public int solution(int n) {
+        fibo[0] = 0;
+        fibo[1] = 1;
+
+        for (int i = 2; i <= n; i++) {
+            int val = fibo[i - 1] + fibo[i - 2];
+            val %= NUM;
+            fibo[i] = val;
+        }
+
+        return fibo[n];
+    }
+}
