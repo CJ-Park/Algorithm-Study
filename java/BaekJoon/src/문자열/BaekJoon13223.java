1ef700de72b3074de03be5eea1678b0b5186c588
CJ-Park <king01286@naver.com>
2025-04-01 20:47:36 +0900

    [백준] 소금 폭탄 / 문자열

---
+++ java/BaekJoon/src/문자열/BaekJoon13223.java
@@ -0,0 +1,52 @@
+package 문자열;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+// 소금 폭탄
+public class BaekJoon13223 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int now = getSeconds(br.readLine());
+        int end = getSeconds(br.readLine());
+        int gap;
+
+        if (end <= now) {
+            gap = end - now + 3600 * 24;
+        } else {
+            gap = end - now;
+        }
+
+        String result = getTime(gap);
+        System.out.println(result);
+    }
+
+    private static String getTime(int seconds) {
+        int hour = seconds / 3600;
+        seconds = seconds % 3600;
+
+        int minute = seconds / 60;
+        seconds = seconds % 60;
+
+        return String.format("%02d:%02d:%02d", hour, minute, seconds);
+    }
+
+    private static int getSeconds(String input) {
+        String[] arr = input.split(":");
+        int time = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            if (i == 0) { // 시간
+                time += Integer.parseInt(arr[i]) * 3600;
+            } else if (i == 1) { // 분
+                time += Integer.parseInt(arr[i]) * 60;
+            } else {
+                time += Integer.parseInt(arr[i]);
+            }
+        }
+
+        return time;
+    }
+}
--- java/BaekJoon/src/문자열/BaekJoon1543.java
+++ java/BaekJoon/src/문자열/BaekJoon1543.java
@@ -7,7 +7,7 @@ import java.io.InputStreamReader;
 // 문서 검색
 public class BaekJoon1543 {
 
-    public static void main(String[] args) throws IOException {
+    public void solution() throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String input = br.readLine();
 
@@ -18,7 +18,7 @@ public class BaekJoon1543 {
     }
 
     // split 의 시간 복잡도 O(n)
-    private static int countSearch(String input, String search) {
+    private int countSearch(String input, String search) {
         input = " " + input + " ";
         String[] split = input.split(search);
         return split.length - 1;
